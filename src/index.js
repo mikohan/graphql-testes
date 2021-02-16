ced72e7978fbbd49a74be1fbd2965ccb9714f74f
mikohan <angara99@gmail.com>
2021-02-16 17:00:45 +0300

    Finished with comments

--- src/index.js
+++ src/index.js
@@ -85,7 +85,11 @@ const resolvers = {
     author(parent, args, ctx, info) {
       return users.find((user) => user.id === parent.author);
     },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => post.id === parent.post);
+    },
   },
+
   User: {
     posts(parent, args, ctx, info) {
       return posts.filter((item) => item.author === parent.id);
