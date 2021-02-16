a55565c92f77c013bae10f258a2f4456c8efbfe2
mikohan <angara99@gmail.com>
2021-02-16 12:17:55 +0300

    Server running

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import { GraphQLServer } from 'graphql-yoga';
 
 // type defination
+const typeDefs = `
+type Query {
+  hello: String!
+}
+`;
 
 // Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This in my first query';
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => console.log('The server is up'));
