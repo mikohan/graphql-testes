eb7cbc4fe35c6da7f206cc655eb3127e863c978d
mikohan <angara99@gmail.com>
2021-02-18 10:18:46 +0300

    Refactor code almost done

--- src/index.js
+++ src/index.js
@@ -9,16 +9,7 @@ import db from './data';
 
 // Resolvers
 
-const resolvers = {
-  Comment: {
-    author(parent, args, { db }, info) {
-      return db.users.find((user) => user.id === parent.author);
-    },
-    post(parent, args, { db }, info) {
-      return db.posts.find((post) => post.id === parent.post);
-    },
-  },
-};
+const resolvers = {};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
