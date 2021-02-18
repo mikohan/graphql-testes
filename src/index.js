23759b3e3d96c2d99c747fb31dcac96b65bfa3e2
mikohan <angara99@gmail.com>
2021-02-18 10:17:22 +0300

    Refactor code almost done

--- src/index.js
+++ src/index.js
@@ -10,29 +10,12 @@ import db from './data';
 // Resolvers
 
 const resolvers = {
-  Post: {
-    author(parent, args, { db }, info) {
-      return db.users.find((user) => user.id === parent.author);
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter((comment) => comment.post === parent.id);
-    },
-  },
   Comment: {
     author(parent, args, { db }, info) {
       return db.users.find((user) => user.id === parent.author);
     },
     post(parent, args, { db }, info) {
       return db.posts.find((post) => post.id === parent.post);
-    },
-  },
-
-  User: {
-    posts(parent, args, { db }, info) {
-      return db.posts.filter((item) => item.author === parent.id);
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter((comment) => comment.author === parent.id);
     },
   },
 };
